18b170905f3ae8e024238ae85bbe0f5ca79191cd
SaadAAkhtar <saakhta@gmail.com>
2019-11-17 18:17:50 -0500

    Hopefully Final Commit

--- cpe-smart-app/src/js/example-smart-app.js
+++ cpe-smart-app/src/js/example-smart-app.js
@@ -8,40 +8,11 @@
     }
 
     function onReady(smart)  {
-      if (smart.hasOwnProperty('patient')) {    
-        //Queries
+      if (smart.hasOwnProperty('patient')) { 
+        
+        //Queries + variables streamline
         var patient = smart.patient;
         var pt = patient.read();
-        
-        var medicationOrder = smart.patient.api.fetchAll({
-                type: 'MedicationOrder',
-                query: {
-                  _count: 5
-                }
-              });
-
-        $.when(pt, medicationOrder).fail(onError);
-        
-        $.when(pt, medicationOrder).done(function(patient, medicationOrder) {
-          console.log(medicationOrder);
-          var p = defaultPatient();
-          var meds = '';
-          var medss = [];
-          var len = medicationOrder.length;
-          
-          for (i = 0; i < len; i++) {
-            var temp = medicationOrder[i].medicationCodeableConcept.text;
-            
-            if (medss.indexOf(temp) === -1) {
-              medss.push(temp);
-              meds = meds + temp + "\n";
-            }
-          }
-          
-          console.log(meds);
-          p.meds = meds;
-          console.log(p.meds);
-        });
         
         var obv = smart.patient.api.fetchAll({
                     type: 'Observation',
@@ -54,19 +25,26 @@
                     }
                   });
         
+        var medicationOrder = smart.patient.api.fetchAll({
+                type: 'MedicationOrder',
+                query: {
+                  _count: 5
+                }
+              });
+        
         //Observation Query Error Handling
-        $.when(pt, obv).fail(onError);
+        $.when(pt, obv, medicationOrder).fail(onError);
 
         //Observation Query Success Handling
-        $.when(pt, obv).done(function(patient, obv) {
+        $.when(pt, obv, medicationOrder).done(function(patient, obv, medicationOrder) {
           //Function streamline
           var byCodes = smart.byCodes(obv, 'code');
           var p = defaultPatient();
           
           //Variable initilizations
-          var height = byCodes('8302-2');
           var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
+          var height = byCodes('8302-2');
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
           var weight = byCodes('29463-7');
@@ -82,8 +60,22 @@
           var married = '';
           var providerName = '';
           var providerRole = '';
+          var meds = '';
+          var medss = [];
+          var len = medicationOrder.length;
           
           //Variable initilization undefined check
+          for (i = 0; i < len; i++) {
+            if (typeof medicationOrder[i] !== 'undefined' && typeof medicationOrder[i].medicationCodeableConcept !== 'undefined') {
+              var temp = medicationOrder[i].medicationCodeableConcept.text;
+            
+              if (medss.indexOf(temp) === -1) {
+                medss.push(temp);
+                meds = meds + temp + "\n";
+              }
+            }
+          }
+          
           if (typeof patient.name[0] !== 'undefined') {
             fname = patient.name[0].given.join(' ');
             lname = patient.name[0].family.join(' ');
@@ -111,6 +103,8 @@
           }
           
           //Prepare variables for index.html
+          p.meds = meds;
+          console.log(p.meds);
           p.birthdate = patient.birthDate;
           p.gender = gender;
           p.fname = fname;
