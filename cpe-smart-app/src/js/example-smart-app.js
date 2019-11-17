62b3127952e8516c6397b83e2e8959468038532a
SaadAAkhtar <saakhta@gmail.com>
2019-11-17 14:57:58 -0500

    Query Check

--- cpe-smart-app/src/js/example-smart-app.js
+++ cpe-smart-app/src/js/example-smart-app.js
@@ -11,7 +11,6 @@
       if (smart.hasOwnProperty('patient')) {
         
         //Queries
-        var med_list = document.getElementById('med_list');
         var patient = smart.patient;
         var pt = patient.read();
         var obv = smart.patient.api.fetchAll({
@@ -25,20 +24,8 @@
                     }
                   });
         
-        smart.patient.read().then(function(patient) {
-          displayPatient (patient);
-        });
-        
-        smart.patient.api.fetchAllWithReferences({type: "MedicationOrder"},["MedicationOrder.medicationReference"]).then(function(results, refs) {
-           results.forEach(function(prescription){
-                if (prescription.medicationCodeableConcept) {
-                    displayMedication(prescription.medicationCodeableConcept.coding);
-                } else if (prescription.medicationReference) {
-                    var med = refs(prescription, prescription.medicationReference);
-                    displayMedication(med && med.code.coding || []);
-                }
-           });
-        });
+        var mobv = smart.patient.api.fetchAllWithReferences({type: "MedicationOrder"},["MedicationOrder.medicationReference"])
+        console.log(mobv);
         
         //Query Error Handling
         $.when(pt, obv).fail(onError);
@@ -134,33 +121,6 @@
     return ret.promise();
 
   };
-  
-  function getPatientName (pt) {
-    if (pt.name) {
-      var names = pt.name.map(function(name) {
-        return name.given.join(" ") + " " + name.family.join(" ");
-      });
-      return names.join(" / ")
-    } else {
-      return "anonymous";
-    }
-  }
-
-  function getMedicationName (medCodings) {
-    var coding = medCodings.find(function(c){
-      return c.system == "http://www.nlm.nih.gov/research/umls/rxnorm";
-    });
-
-    return coding && coding.display || "Unnamed Medication(TM)"
-  }
-
-  function displayPatient (pt) {
-    document.getElementById('patient_name').innerHTML = getPatientName(pt);
-  }
-
-  function displayMedication (medCodings) {
-    med_list.innerHTML += "<li> " + getMedicationName(medCodings) + "</li>";
-  }
 
   function defaultPatient(){
     return {
